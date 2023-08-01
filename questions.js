8dc97c61180774e5a0572225c2edfeb00395054a
Muriel MARCHAL <muriel.marchal@hotmail.com>
2023-08-01 13:35:36 +0200

    add questions

--- questions.js
+++ questions.js
@@ -45,5 +45,86 @@ const questions = [{
         title: 'Dragon',
         house: 'slytherin'
     }]
+}, 
+{
+    question: 'Quel est ton lieu favori à Poudlard ?',
+    answers: [{
+        title: 'la bibliothèque',
+        house: 'ravenclaw'
+    }, {
+        title: 'la forêt interdite',
+        house: 'hufflepuff'
+    }, {
+        title: 'les cachots',
+        house: 'slytherin'
+    }, {
+        title: 'la cabane de Hagrid',
+        house: 'gryffondor'
+    }]
+}, 
+{
+    question: 'Quel est ton moment magique favori ?',
+    answers: [{
+        title: 'la diversion du troll',
+        house: 'ravenclaw'
+    }, {
+        title: 'le cambriolage de Gringotts',
+        house: 'gryffondor'
+    }, {
+        title: 'la découverte du miroir',
+        house: 'slytherin'
+    }, {
+        title: 'le match de quidditch',
+        house: 'hufflepuff'
+    }]
+},
+{
+    question: 'Quel est ton objet magique favori ?',
+    answers: [{
+        title: 'la pierre philosophale',
+        house: 'slytherin'
+    }, {
+        title: "la cape d'invisibilité",
+        house: 'gryffondor'
+    },
+    {
+        title: 'le miroir du Risèd',
+        house: 'hufflepuff'
+    }, {
+        title: 'la coupe de feu',
+        house: 'ravenclaw'
+    }]
+},
+{
+    question: 'Quel est ton détecteur magique favori ?',
+    answers: [{
+        title: 'le capteur de dissimulation',
+        house: 'ravenclaw'
+    }, {
+        title: 'le scrutoscope',
+        house: 'gryffondor'
+    }, {
+        title: "la glace à l'ennemi",
+        house: 'hufflepuff'
+    }, {
+        title: 'le rapeltout',
+        house: 'slytherin'
+    }]
+},
+{
+    question: 'Quel est ton wagon dans le Pourlard Express ?',
+    answers: [{
+        title: 'Wagon 1',
+        house: 'hufflepuff'
+    }, {
+        title: 'Wagon 2',
+        house: 'gryffondor'
+    }, {
+        title: 'Wagon 3',
+        house: 'ravenclaw'
+    }, {
+        title: 'Wagon 4',
+        house: 'slytherin'
+    }]
 }
 ];
